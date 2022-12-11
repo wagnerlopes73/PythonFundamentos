fbe07af5b1c8f57d2f3414442c397e81483708bc
wagnerlopes73 <51516628+wagnerlopes73@users.noreply.github.com>
2022-12-11 16:29:09 -0300

    ok

---
+++ Cap02/Notebooks/DSA-Python-Cap02-01-Numeros.py
@@ -0,0 +1,4 @@
+
+a = int(5.5)
+type(a)
+print (a)
---
+++ Cap02/Notebooks/DSA-Python-Cap02-03-Strings.py
@@ -0,0 +1,23 @@
+a = "Ary Wagner de Oliveira Lopes"
+
+# Indexação em Python começa com 0
+
+
+# indexa o quarto caractere da string, começando pelo 0
+b = a[4]
+
+# Imprimi o caractere
+print(b)
+
+# Imprimi a partir do caractere
+c = a[1:]
+print(c)
+
+# Imprimi até do caractere
+d = a[:4]
+print(d)
+
+# Imprimi tudo em lower case
+e = a.lower()
+print(e)
+
